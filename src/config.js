1ad96e5fa8559c0e2d4febe606bf6ee1d1ba9a1a
nasjones <nasjonez1@gmail.com>
2020-06-30 22:35:24 -0400

    decoding update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 module.exports = {
-    ENDPOINT: 'http://localhost:8000/api',
-    // 'https://afternoon-brook-09755.herokuapp.com/api',
+    ENDPOINT: 'https://afternoon-brook-09755.herokuapp.com/api',
     REACT_APP_API_KEY: process.env.REACT_APP_API_KEY
 }

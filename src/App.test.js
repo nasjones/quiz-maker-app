52a5c45d791edcccdb3f3a1fc17e6fb6f3572509
nasjones <46979749+nasjones@users.noreply.github.com>
2023-01-25 14:15:43 -0500

    update to use root

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import App from './App';
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+it("renders without crashing", () => {
+	const div = document.createElement("div");
+	const root = ReactDOM.createRoot(div);
+	root.render(
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	);
+	ReactDOM.unmountComponentAtNode(div);
 });

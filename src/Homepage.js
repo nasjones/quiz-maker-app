0dd7e7a33a174e5402c491702a8db8bade35a1f5
nasjones <46979749+nasjones@users.noreply.github.com>
2023-01-25 15:09:15 -0500

    fix routes

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Foot from './foot'
-import './Homepage.css';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Foot from "./foot";
+import "./Homepage.css";
 
 export default class Homepage extends Component {
-
-    render() {
-        return (
-            <div id='homepage'>
-                {/* <div className="user-options">
-                    <Link to={'/signup'}>Signup</Link> / <Link to={'/login'}>Login</Link>
-                </div> */}
-                <h1 className='landingTitle'>QUIZ BOWL</h1>
-                <div className='buttonWrap'>
-                    <Link to={'/createPrompt'} className='homeNav greenButton' >CREATE YOUR OWN QUIZ</Link>
-                    <br />
-                    <Link to={'/existing-quizzes'} className='homeNav yellowButton'>EXPLORE OUR QUIZZES</Link>
-                </div>
-                <Foot />
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div id="homepage">
+				<div className="user-options">
+					<Link to={"/signup"}>Signup</Link> / <Link to={"/login"}>Login</Link>
+				</div>
+				<h1 className="landingTitle">QUIZ BOWL</h1>
+				<div className="buttonWrap">
+					<Link to={"/createPrompt"} className="homeNav greenButton">
+						CREATE YOUR OWN QUIZ
+					</Link>
+					<br />
+					<Link to={"/existing-quizzes"} className="homeNav yellowButton">
+						EXPLORE OUR QUIZZES
+					</Link>
+				</div>
+				<Foot />
+			</div>
+		);
+	}
 }

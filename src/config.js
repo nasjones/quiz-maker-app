465927806541d61c87c5a27679a3adca5242cde5
nasjones <46979749+nasjones@users.noreply.github.com>
2023-01-25 13:25:56 -0500

    Update config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 module.exports = {
-	ENDPOINT: "https://afternoon-brook-09755.herokuapp.com/api",
+	ENDPOINT: process.env.API_URL,
 	REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
 };

51032d5b9049397cc3e64adc87791d5afb94436e
nasjones <46979749+nasjones@users.noreply.github.com>
2023-01-25 14:42:12 -0500

    Remove dotenv

--- src/config.js
+++ src/config.js
@@ -1,14 +1,4 @@
-require("dotenv").config();
 module.exports = {
-	ENDPOINT: process.env.API_URL,
+	ENDPOINT: process.env.REACT_APP_API_URL,
 	REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
-	webpack: (config, { isServer }) => {
-		// Fixes npm packages that depend on `fs` module
-		if (!isServer) {
-			config.node = {
-				fs: "empty",
-			};
-		}
-		return config;
-	},
 };

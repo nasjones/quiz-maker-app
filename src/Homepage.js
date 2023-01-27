e9699b3fa471773a970ca07c10b3c484b460291c
nasjones <46979749+nasjones@users.noreply.github.com>
2023-01-27 10:45:11 -0500

    Title cleanup

--- src/Homepage.js
+++ src/Homepage.js
@@ -7,10 +7,7 @@ export default class Homepage extends Component {
 	render() {
 		return (
 			<div id="homepage">
-				{/* <div className="user-options">
-					<Link to={"/signup"}>Signup</Link> / <Link to={"/login"}>Login</Link>
-				</div> */}
-				<h1 className="landingTitle">QUIZ BOWLs</h1>
+				<h1 className="landingTitle">QUIZ BOWL</h1>
 				<div className="buttonWrap">
 					<Link to={"/createPrompt"} className="homeNav greenButton">
 						CREATE YOUR OWN QUIZ
